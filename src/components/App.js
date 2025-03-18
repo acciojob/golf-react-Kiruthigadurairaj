f87d248d3ab003df1bf0a8e7e3293eb30e112ab3
kiruthiga durairaj <kiruthigalatha05@gmail.com>
2025-03-18 20:25:28 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,43 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "../styles/App.css";
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             renderBall: false,
-            posi : 0,
             ballPosition: { left: "0px" }
         };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-    };
-
-    buttonClickHandler() {
-   
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
+        this.buttonClickHandler = this.buttonClickHandler.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
     }
 
-    // bind ArrowRight keydown event
+    buttonClickHandler() {
+        this.setState({ renderBall: true });
+        document.addEventListener("keydown", this.handleKeyDown);
+    }
+
+    handleKeyDown(event) {
+        if (event.keyCode === 39) { // Right arrow key
+            const newPos = parseInt(this.state.ballPosition.left) + 5;
+            this.setState({ ballPosition: { left: `${newPos}px` } });
+        }
+    }
+
     componentDidMount() {
-      
+        // No need to add event listener here since it's added on button click
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("keydown", this.handleKeyDown);
+    }
+
+    renderBallOrButton() {
+        return this.state.renderBall ? (
+            <div className="ball" style={this.state.ballPosition}></div>
+        ) : (
+            <button className="start" onClick={this.buttonClickHandler}>Start</button>
+        );
     }
 
     render() {
@@ -34,9 +45,8 @@ class App extends Component {
             <div className="playground">
                 {this.renderBallOrButton()}
             </div>
-        )
+        );
     }
 }
-
 
 export default App;
